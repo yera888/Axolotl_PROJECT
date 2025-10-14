479453a35375efc9e9586a0dba875d2507ca9c37
yera888 <yeraldinet1234@gmail.com>
2025-10-14 18:36:33 -0400

    Initial commit

---
+++ src/main/java/com/csc340/axolotl_crud_api/AxolotlCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.axolotl_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxolotlCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxolotlCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc340/axolotl_crud_api/AxolotlCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.axolotl_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxolotlCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
